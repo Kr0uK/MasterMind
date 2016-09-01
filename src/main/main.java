76c89328ecf983cf09ce1a44b9189a62114b06c2
Kr0uK <boris.motz@gmail.com>
2016-09-01 11:14:46 +0200

    test

--- src/main/main.java
+++ src/main/main.java
@@ -8,38 +8,40 @@ import java.util.Scanner;
 public class main {
 
     final static String[] COULEURS = {"Rouge", "Jaune", "Vert", "Bleu", "Orange", "Blanc", "Violet", "Fuchsia"};
-    static int[] tProp;
-    static int[] tSolution;
+    static int[] tProp = new int[4];
+    static int[] tSolution = new int[4];
     static int i = 0;
     static int j = 0;
     static int essais = 0;
     static int tailleTableau = 4;
     static int choix = 0;
     static Scanner sc = new Scanner(System.in);
-    static Boolean[] placeCorrecte;
-    static Boolean[] couleurCorrecte;
+    static Boolean[] placeCorrecte = new Boolean[4];
+    static Boolean[] couleurCorrecte = new Boolean[4];
 
 
     public static void main(String[] args) { // Menu principal
 
-        affichercouleurs(); // on affiche les couleurs et leur code
-        choixmode(); // choix du mode de jeu
+       // afficherCouleurs(); // on affiche les couleurs et leur code
+        choixMode(); // choix du mode de jeu
 
     }
 
 
-    private static void choixmode() {
+    private static void choixMode() {
         System.out.println("MasterMind. \n Mode 1 : Le système génère une combinaison à trouver. \n" +
                 " Mode 2 : Vous choisissez une combinaison et le système essaye de la trouver.");
 
         while (choix != 1 && choix != 2) {
             choix = Integer.parseInt(String.valueOf(sc.nextInt()));
             if (choix == 1) { // la machine génère une solution et l'humain doit le trouver (Mastermind classique)
-                generersolution();
+                afficherCouleurs();
+                genererSolution();
                 reponseHumain();
             } else if (choix == 2) { // Le joueur génère une solution et la machine la devine
-                generersolutionHumain();
-                resoudresolution();
+                afficherCouleurs();
+                genererSolutionHumain();
+                resoudreSolution();
             } else {
                 System.out.println("Veuillez entrer un nombre correct.");
             }
@@ -47,38 +49,38 @@ public class main {
 
     }
 
-    private static void generersolutionHumain() { // Le joueur propose une combinaison
+    private static void genererSolutionHumain() { // Le joueur propose une combinaison
 
         int couleur;
-        int[] coulTest = {9, 9, 9, 9};
-        for (i = 0; i <= tailleTableau; i++) // Pour chaque emplacement de tableau
+        int[] coulTest = new int[4];
+        for (i = 0; i < tailleTableau; i++) // Pour chaque emplacement de tableau
         {
-            System.out.println("Veuillez rentrer une couleur valide");
+            System.out.println("Veuillez rentrer une couleur valide (entre 0 et 7)");
             couleur = Integer.parseInt(String.valueOf(sc.nextInt()));
             while (couleur < 0 && couleur > COULEURS.length - 1) {
-                System.out.println("Veuillez rentrer une couleur valide");
+                System.out.println("Veuillez rentrer une couleur valide (entre 0 et 7)");
                 couleur = Integer.parseInt(String.valueOf(sc.nextInt()));
                 coulTest[i] = couleur;
                 for (int j = 0; j < coulTest.length; i++) {
                     if (couleur != coulTest[i]) {
                         tSolution[i] = couleur;
                     } else {
-                        coulTest[i] = 9;
-                        couleur = 9;
+
                     }
                 }
 
             }
-
-
+            for (int i:coulTest) {
+                System.out.println(coulTest[i]);
+            }
         }
     }
 
     private static void reponseHumain() { // Le joueur tente de trouver la combinaison générée par la machine
-
+        //TODO
     }
 
-    private static void affichercouleurs() { // Permet l'affichage des codes et couleurs
+    private static void afficherCouleurs() { // Permet l'affichage des codes et couleurs
         int k = 0;
         for (String couleur : COULEURS) {
             System.out.println(k + " : " + couleur);
@@ -87,13 +89,15 @@ public class main {
 
     }
 
-    private static void resoudresolution() {// la machine tente de résoudre une combinaison
+    private static void resoudreSolution() {// la machine tente de résoudre une combinaison
+        //TODO
     }
 
-    private static void generersolution() { // la machine génère une combinaison
+
+    private static void genererSolution() { // la machine génère une combinaison
 
         int rep1, rep2, rep3, rep4;
-
+        //TODO
 
     }
 
